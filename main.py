83605b12e37772f73207aa5dd5659f52fc3d7c95
subramanyakrishna <subramanyakrishna2000@gmail.com>
2021-09-19 22:52:41 +0530

    added fuction to check for xpath

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import Select
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.common.exceptions import NoSuchElementException
+
 import os
 import time
 
@@ -42,50 +44,54 @@ def w(t):
     time.sleep(t)
 
 
-try:
+def check_exists_by_xpath(xpath):
+    try:
+        driver.find_element_by_xpath(xpath)
+    except NoSuchElementException:
+        return False
+    return True
+
+
+# wait for login page or direct tweet page to arrive
+main = WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.ID, 'react-root')))
+w(3)
+if(check_exists_by_xpath(EMAIL_INPUT_XPATH)):
+    email_input = driver.find_element_by_xpath(EMAIL_INPUT_XPATH)
+    email_input.send_keys(email)
+    password_input = driver.find_element_by_xpath(PSWD_INPUT_XPATH)
+    password_input.send_keys(password)
+    password_input.submit()
+    # wait for the login to complete
     main = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, 'react-root')))
-    w(3)
-    # email_input = main.find_element_by_xpath(EMAIL_INPUT_XPATH)
+    w(2)
 
-    # email_input.send_keys(email)
-    # password_input = main.find_element_by_xpath(PSWD_INPUT_XPATH)
-    # password_input.send_keys(password)
-    # password_input.submit()
-    # w(3)
-    try:
-        main = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, 'react-root')))
-        w(2)
-        tweet = "Hey, This is tweet by Selenium"
-        tweetInputField = driver.find_element_by_xpath(TWEET_INPUT_FIELD)
-        tweetInputField.send_keys(tweet)
-        w(2)
-        scheduleButton = driver.find_element_by_xpath(SCHEDULE_BUTTON)
-        scheduleButton.click()
+tweet = "Hey, This is tweet by Selenium"
+tweetInputField = driver.find_element_by_xpath(TWEET_INPUT_FIELD)
+tweetInputField.send_keys(tweet)
+w(2)
+scheduleButton = driver.find_element_by_xpath(SCHEDULE_BUTTON)
+scheduleButton.click()
 
-        w(2)
-        selectHour = driver.find_element_by_xpath(SELECT_HOUR)
-        w(1)
-        selectHour.send_keys("4")
-        w(2)
-        selectMinite = driver.find_element_by_xpath(SELECT_MINUTE)
-        w(1)
+w(2)
+selectHour = driver.find_element_by_xpath(SELECT_HOUR)
+w(1)
+selectHour.send_keys("4")
+w(2)
+selectMinite = driver.find_element_by_xpath(SELECT_MINUTE)
+w(1)
 
-        selectMinite.send_keys("59")
-        w(2)
-        selectAmPm = driver.find_element_by_xpath(SELECT_AM_PM)
-        w(1)
-        selectAmPm.send_keys("AM")
-        w(2)
-        scheduleSubmit = driver.find_element_by_xpath(SCHEDULE_SUBMIT)
-        w(1)
-        scheduleSubmit.click()
-        w(2)
-        tweetButton = driver.find_element_by_xpath(
-        )
-        tweetButton.click()
-    except:
-        print("Error while tweeting")
-except:
-    print("Error while logging in")
+selectMinite.send_keys("59")
+w(2)
+selectAmPm = driver.find_element_by_xpath(SELECT_AM_PM)
+w(1)
+selectAmPm.send_keys("AM")
+w(2)
+scheduleSubmit = driver.find_element_by_xpath(SCHEDULE_SUBMIT)
+w(1)
+scheduleSubmit.click()
+w(2)
+tweetButton = driver.find_element_by_xpath(
+)
+tweetButton.click()
